30e6bd12f06ff5416ccf618582fec19cd0651e82
rlzijdeman <richard.zijdeman@iisg.nl>
2022-03-31 15:56:18 +0200

    materials for intro linked datastories

--- retrieve-queries-via-.R
+++ retrieve-queries-via-.R
@@ -7,8 +7,11 @@ library(jsonlite) # for working with JSON
 URL = "http://grlc.io/api-git/rlzijdeman/sparql-queries/voyages-subvoyages-overview?endpoint=https%3A%2F%2Fapi.druid.datalegend.net%2Fdatasets%2FESTA-DEMO%2Festa-demo-viz%2Fservices%2Festa-demo-viz%2Fsparql"
 df <- GET(URL)
 df <- fromJSON(content(df, as = 'text', encoding = "UTF-8"))
+summary(df)
 
 # demo
 URL2 = "http://grlc.io/api-git/rlzijdeman/sparql-queries/esta-demo-voyage?endpoint=https%3A%2F%2Fapi.druid.datalegend.net%2Fdatasets%2FESTA-DEMO%2Festa-demo-viz%2Fservices%2Festa-demo-viz%2Fsparql"
-data <- GET(URL2)
-data <- fromJSON(content(data, as = 'text', encoding = "UTF-8"))
+df2 <- GET(URL2)
+df2 <- fromJSON(content(df2, as = 'text', encoding = "UTF-8"))
+summary(df2)
+
